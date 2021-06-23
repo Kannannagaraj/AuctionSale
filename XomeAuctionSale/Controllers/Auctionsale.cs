92efcc262e21eeb60bf9b00371dad43f9f3bc492
Kannan Nagaraj <n.kannan3791@gmail.com>
2021-06-23 15:36:01 +0530

    Source code added

---
+++ XomeAuctionSale/Controllers/Auctionsale.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuctionBussinessLogic;
+using AuctionServiceLayer;
+//using System.Web.Http;
+using System.Net;
+
+namespace XomeAuctionSale.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Auctionsale : ControllerBase
+    {
+        private IAuctionSale _auctionSale;
+        public Auctionsale(IAuctionSale auctionSale)
+        {
+            this._auctionSale = auctionSale;
+        }
+
+        /// <summary>
+        /// Get list of auction event details
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetAuctionEventDetails")]
+
+        public async Task<IActionResult> GetAuctionEventDetails()
+        {
+            var lstAuctionDetails = _auctionSale.GetAuctionEventDetails();
+
+            if (lstAuctionDetails.Count > 0)
+                return await Task.FromResult(Ok(new { lstAuctionDetails, message = ConstantInfos.SuccessMessage }));
+            else
+                return await Task.FromResult(Ok(new { message = ConstantInfos.NoRecordMessage }));
+        }
+
+        /// <summary>
+        /// make bid for auction based on auction id
+        /// </summary>
+        /// <param name="auctionEventId"></param>
+        /// <param name="bidDetails"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("BidForAuctionEvent")]
+
+        public async Task<IActionResult> BidForAuctionEvent(int auctionEventId, BidDetails bidDetails)
+        {
+            var bid = _auctionSale.BidForAuctionEvent(auctionEventId, bidDetails);
+            return await Task.FromResult(Ok(new { bid, message = ConstantInfos.BidSuccessMessage }));
+
+        }
+
+
+        /// <summary>
+        /// Get the winner of the auction sale
+        /// </summary>
+        /// <param name="auctionSalesDetails"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("GetAuctionWinningAmount1")]
+
+        public async Task<IActionResult> GetAuctionWinningAmount1(AuctionSalesDetails auctionSalesDetails)
+        {
+            if(auctionSalesDetails.BidDetails.Count>0)
+            {
+                var minBidPrice = auctionSalesDetails.BidDetails.Min(x => x.StartBidAmount);
+                if(minBidPrice<auctionSalesDetails.BasePrice)
+                    return await Task.FromResult(Ok(new { message = ConstantInfos.BasePriceValidationMessage }));
+                if(auctionSalesDetails.BidDetails.Count>auctionSalesDetails.MaxBidderCount)
+                    return await Task.FromResult(Ok(new {  message = ConstantInfos.BidderCountValidationMessage }));
+            }
+            var bidWinningAmount = _auctionSale.GetWinnerBidAmount1(auctionSalesDetails);
+            return await Task.FromResult(Ok(new { bidWinningAmount, message = ConstantInfos.SuccessMessage }));
+
+        }
+        /// <summary>
+        /// Get the winner of the auction sale
+        /// </summary>
+        /// <param name="auctionSalesDetails"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("GetAuctionWinningAmount2")]
+
+        public async Task<IActionResult> GetAuctionWinningAmount2(AuctionSalesDetails auctionSalesDetails)
+        {
+            if (auctionSalesDetails.BidDetails.Count > 0)
+            {
+                var minBidPrice = auctionSalesDetails.BidDetails.Min(x => x.StartBidAmount);
+                if (minBidPrice < auctionSalesDetails.BasePrice)
+                    return await Task.FromResult(Ok(new { message = ConstantInfos.BasePriceValidationMessage }));
+                if (auctionSalesDetails.BidDetails.Count > auctionSalesDetails.MaxBidderCount)
+                    return await Task.FromResult(Ok(new { message = ConstantInfos.BidderCountValidationMessage }));
+            }
+            var bidWinningAmount = _auctionSale.GetWinnerBidAmount2(auctionSalesDetails);
+            return await Task.FromResult(Ok(new { bidWinningAmount, message = ConstantInfos.SuccessMessage }));
+
+        }
+    }
+}
---
+++ XomeAuctionSale/Startup.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AuctionServiceLayer;
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+
+namespace XomeAuctionSale
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddTransient<IAuctionSale, AuctionSale>();
+            services.AddTransient(typeof(IAuctionSale), typeof(AuctionSale));
+            services.AddSwaggerGen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Auction Sale Api v1");
+            });
+            app.UseExceptionHandler(
+                options =>
+                {
+                    options.Run(
+                        async context =>
+                        {
+                            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                            context.Response.ContentType = "application/json";
+                            var exceptionObject = context.Features.Get<IExceptionHandlerFeature>();
+                            if (null != exceptionObject)
+                            {
+                                var errorMessage = $"Exception Error: {exceptionObject.Error.Message}";
+                                await context.Response.WriteAsync(errorMessage).ConfigureAwait(false);
+                            }
+                        });
+                }
+            );
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
